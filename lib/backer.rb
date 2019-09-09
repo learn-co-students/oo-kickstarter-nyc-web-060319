e5c7ead6bd7c3d01974638bd8c5b231b69891e04
github username <github email address>
2019-09-09 01:01:30 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,14 @@
 class Backer
-    attr_reader :name
-    attr_accessor :backed_projects
+    attr_reader :name, :backed_projects
 
-def initialize(name)
-    @name = name
-    @backed_projects = []
-end
+    def initialize(name)
+        @name = name
+        @backed_projects = []
+    end
 
-def back_project(project)
-    @backed_projects << project
-    project.add_backer(self) unless project.backers.include? self
-end
+    def back_project(project)
+        backed_projects << project
+        project.backers << self
+    end
 
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,14 @@
 class Project
+    attr_reader :title, :backers
 
-    attr_reader :name, :backers, :backed_projects, :title
-
-    def initialize (title)
+    def initialize(title)
         @title = title
         @backers = []
-        @backed_projects = backed_projects
     end
 
     def add_backer(backer)
-        @backers << backer
+        backers << backer
         backer.backed_projects << self
     end
+
 end
